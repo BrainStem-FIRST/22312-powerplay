edfa527583b01d38aba0081252b0c5ccf6dab7df
0zgeada <uzmanoa@gmail.com>
2022-11-05 15:56:14 -0400

    Update Auto
    finetuning some values and adding splines

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Auto.java
@@ -26,7 +26,8 @@ public class Auto extends LinearOpMode {
     // 1: Synchronous
     // 2: Speed set
     public static int TEST_NUMBER = 2;
-    public static double SPEED = 15.0;    // slower speed, in/sec
+    public static double SPEED = 50.0;    // slower speed, in/sec
+    public static int PARKING_POSITION = 1;
 
     @Override
     public void runOpMode() {
@@ -57,7 +58,7 @@ public class Auto extends LinearOpMode {
         // adjust the coordinate system accordingly.
         Trajectory trajectory1, trajectory2;
 
-        Pose2d startingPose = new Pose2d(0,0,Math.toRadians(90)); // On Red Wall facing Blue Wall
+        Pose2d startingPose = new Pose2d(-36,-67.25,Math.toRadians(90)); // On Red Wall facing Blue Wall
         robot.drive.setPoseEstimate(startingPose);  // Needed to be called once before the first trajectory
 
         //define trajectories
@@ -66,15 +67,15 @@ public class Auto extends LinearOpMode {
                 // From starting position of RED-LEFT; relative movements to starting position
                 trajectory1 = robot.drive.trajectoryBuilder(startingPose)
                         //moves forward in a line by 40 in and faces 90 degrees away by end
-                        .lineToLinearHeading(new Pose2d(0, 40, Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(90)))
                         //turns left and ends up 40 inches to the right, heading facing cones
-                        .splineTo(new Vector2d(-40, 40), Math.toRadians(180))
+                        .splineTo(new Vector2d(-24, 48), Math.toRadians(180))
                         .build();
 
                 // starting from where trajectory1 ended up, assume cone is grabbed, move back to depositing position
                 trajectory2 = robot.drive.trajectoryBuilder(trajectory1.end(), true)
                         //moves backwards in a line by 40 inches on x-axis; still faces the cones
-                        .lineToLinearHeading(new Pose2d(0, 40, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(0)))
                         .build();
 
                 runTest = true;
@@ -87,19 +88,28 @@ public class Auto extends LinearOpMode {
                 // From starting position of RED-LEFT; relative movements to starting position
                 trajectory1 = robot.drive.trajectoryBuilder(startingPose)
                         //moves forward in a line by 40 in and faces 90 degrees away by end
-                        .lineToLinearHeading(new Pose2d(0, 40, Math.toRadians(90)),
+                        .splineTo(new Vector2d(-36, -24), Math.toRadians(90),
                                 SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL))
-                        //turns left and ends up 40 inches to the right, heading facing cones
-                        .splineTo(new Vector2d(-40, 40), Math.toRadians(180),
+                        .splineTo(new Vector2d(-54, -16), Math.toRadians(180),
+                                SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL))
+                        .splineTo(new Vector2d(-64.25, -16), Math.toRadians(180),
                                 SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL))
                         .build();
+                        /* .lineToLinearHeading(new Pose2d(-36, -14, Math.toRadians(180)),
+                                SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL))
+                        //turns left and ends up 40 inches to the right, heading facing cones
+                        .splineTo(new Vector2d(-64.25, -14), Math.toRadians(180),
+                                SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
+                                SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL)) */
 
                 // starting from where trajectory1 ended up, assume cone is grabbed, move back to depositing position
                 trajectory2 = robot.drive.trajectoryBuilder(trajectory1.end(), true)
                         //moves backwards in a line by 40 inches on x-axis; still faces the cones
-                        .lineToLinearHeading(new Pose2d(0, 40, Math.toRadians(0)),
+                        .lineToLinearHeading(new Pose2d(-24, -14, Math.toRadians(180)),
                                 SampleMecanumDrive.getVelocityConstraint(SPEED, MAX_ANG_VEL, TRACK_WIDTH),
                                 SampleMecanumDrive.getAccelerationConstraint(MAX_ACCEL))
                         .build();
@@ -127,14 +137,30 @@ public class Auto extends LinearOpMode {
 
             // TODO: Grab the cone, lift, etc. Need to set state and update once merged to working Main branch
             // engage cone grabbing sequence
-            stateMap.put(robot.lift.LIFT_SUBHEIGHT, robot.lift.APPROACH_HEIGHT);
-            robot.updateSystems();
+            /* stateMap.put(robot.lift.LIFT_SUBHEIGHT, robot.lift.APPROACH_HEIGHT);
+            robot.updateSystems(); */
+            sleep(1000);
 
             robot.drive.followTrajectory(trajectory2);
 
             // engage cone depositing sequence
-            stateMap.put(robot.lift.LIFT_SUBHEIGHT, robot.lift.PLACEMENT_HEIGHT);
-            robot.updateSystems();
+            /* stateMap.put(robot.lift.LIFT_SUBHEIGHT, robot.lift.PLACEMENT_HEIGHT);
+            robot.updateSystems(); */
+            sleep (1000);
+
+            runTest = false;
+
+            /********************************
+            if (elapsedTime > timeToPark){
+                // stop trajectory
+                // determine robot position
+                // switch statement
+                // for parking lot one, x position is between minus something to minus something
+                // for parking lot two, the same but the somethings are different values
+                // for parking lot three, same as parking lot two
+                // TODO: figure out values for parking lots
+            }
+            ********************************/
         }
     }
 }
