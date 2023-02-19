c746487c809e5ec8a743867920de2c4e9831bccc
Ozge Ada <uzmanoa@gmail.com>
2023-02-19 10:10:15 -0500

    DropCone adjustment to avoid hitting top of pole.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autoclasses/BrainStemRobotA.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/autoclasses/BrainStemRobotA.java
@@ -147,7 +147,7 @@ public class BrainStemRobotA {
     }
 
     public void dropCone() throws InterruptedException {
-        lift.raiseHeightTo(lift.getPosition() - 300);
+        lift.raiseHeightTo(lift.getPosition() - 200);
         sleep(100); // was 100
         grabber.grabberOpen();
         sleep(100); // was 100
