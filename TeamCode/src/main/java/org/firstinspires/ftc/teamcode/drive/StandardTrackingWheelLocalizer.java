1fb1bb48b35e420cb7190d6281c7cb31e74178c2
Ozge Ada <uzmanoa@gmail.com>
2023-04-06 07:58:36 -0400

    Fixed encoder assignment in Localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -49,9 +49,9 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
                 new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
         ));
 
-        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "BL"));  //left odometry
+        leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FL"));  //left odometry
         rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FR")); //right odometry
-        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FL")); //back odometry
+        frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "BL")); //back odometry
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
         //Encoder.setDirection(Encoder.Direction.REVERSE);
